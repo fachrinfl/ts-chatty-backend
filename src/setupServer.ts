ecae95321c0dff9ca05aea1882b0d5cbbf535b07
Fachri Naufal <fachrinfl@192.168.1.21>
2022-12-25 15:17:15 +0700

    setup socketio with redis adapter

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -5,6 +5,9 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import cookieSession from 'cookie-session';
 import compression from 'compression';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 import { config } from './config';
@@ -55,20 +58,39 @@ export class ChattyServer {
 
     private globalErrorHandler(app: Application): void {}
 
-    private startServer(app: Application): void {
+    private async startServer(app: Application): Promise<void> {
         try {
             const httpServer: http.Server = new http.Server(app);
-            this.startHttpServer(httpServer)
+            const socketIO: Server = await this.createSocketIO(httpServer);
+            this.startHttpServer(httpServer);
+            this.socketIOConnection(socketIO);
         } catch (error) {
             console.log(error);
         }
     }
 
-    private createSocketIO(httpServer: http.Server): void {}
+    private async createSocketIO(httpServer: http.Server): Promise<Server> {
+        const io: Server = new Server(httpServer, {
+            cors: {
+                origin: config.CLIENT_URL,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            }
+        });
+        const pubClient = createClient({ url: config.REDIS_HOST });
+        const subClient = pubClient.duplicate();
+        await Promise.all([pubClient.connect(), subClient. connect()]);
+        io.adapter(createAdapter(pubClient, subClient));
+        return io;
+    }
 
     private startHttpServer(httpServer: http.Server): void {
+        console.log(`Server has started with process ${process.pid}`);
         httpServer.listen(SERVER_PORT, () => {
             console.log(`Server running on port ${SERVER_PORT}`);
         });
+    }
+
+    private socketIOConnection(io: Server): void {
+
     }
 }
