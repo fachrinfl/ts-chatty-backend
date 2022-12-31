59e0aed132e5a94bd43a4f600cc1cc7af19023f7
Fachri Naufal <fachrinfl@Fachris-MacBook-Pro.local>
2022-12-31 22:11:06 +0700

    create signup method

---
+++ src/features/auth/controllers/signup.ts
@@ -0,0 +1,52 @@
+import { UploadApiResponse } from 'cloudinary';
+import { ISignUpData } from './../interfaces/auth.interface';
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { authServices } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { upload } from '@global/helpers/cloudinary-upload';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+    const checkIfUserExist: IAuthDocument = await authServices.getUserByUsernameOrEmail(username, email);
+
+    if (checkIfUserExist) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = SignUp.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+    const result: UploadApiResponse = await upload(avatarImage, `${userObjectId}`, true, true) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError('File upload: Error occurated. Try again.');
+    }
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor} = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLatterUppercase(username),
+      email: Helpers.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument;
+  }
+}
