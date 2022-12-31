5333066b9117701dc5a6d2f9cd654821ec2eba2a
Fachri Naufal <fachrinfl@Fachris-MacBook-Pro.local>
2022-12-31 22:40:00 +0700

    update signup auth routes

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,3 +1,4 @@
+import HTTP_STATUS from 'http-status-codes';
 import { UploadApiResponse } from 'cloudinary';
 import { ISignUpData } from './../interfaces/auth.interface';
 import { ObjectId } from 'mongodb';
@@ -35,6 +36,11 @@ export class SignUp {
     if (!result?.public_id) {
       throw new BadRequestError('File upload: Error occurated. Try again.');
     }
+
+    res.status(HTTP_STATUS.CREATED).json({
+      message: 'User created successfully',
+      authData
+    });
   }
 
   private signupData(data: ISignUpData): IAuthDocument {
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-function */
+import { authRoutes } from '@auth/routes/authRoutes';
 import { Application } from 'express';
 
-export default (app: Application) => {
-  const routes = () => {};
+const BASE_PATH = '/api/v1';
 
+export default (app: Application) => {
+  const routes = () => {
+    app.use(BASE_PATH, authRoutes.routes());
+  };
   routes();
 };
