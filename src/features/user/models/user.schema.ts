e40898ce14bea361b41459bfe8939c72cd53f4a0
Fachri Naufal <fachrinfl@192.168.1.5>
2023-01-22 11:25:26 +0700

    fixing type user scheme

--- src/features/user/models/user.schema.ts
+++ src/features/user/models/user.schema.ts
@@ -3,7 +3,7 @@ import mongoose, { model, Model, Schema } from 'mongoose';
 
 const userSchema: Schema = new Schema({
   authId: { type: mongoose.Schema.Types.ObjectId, ref: 'Auth', index: true },
-  profilePicture: { type: Number, default: 0 },
+  profilePicture: { type: String, default: 0 },
   postsCount: { type: Number, default: 0 },
   followersCount: { type: Number, default: 0 },
   followingCount: { type: Number, default: 0 },
