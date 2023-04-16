09a69f887d6e20fead38633fb86ebbfd7468c93c
Fachri Naufal <fachrinfl@Fachris-MacBook-Pro.local>
2023-04-16 19:58:44 +0700

    create method delete in post controller

--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Create } from '@post/controllers/create-post';
 import { Get } from '@post/controllers/get-posts';
+import { Delete } from '@post/controllers/delete-post';
 
 class PostRoutes {
   private router: Router;
@@ -13,8 +14,11 @@ class PostRoutes {
   public routes(): Router {
     this.router.get('/post/all/:page', authMiddleware.checkAuthentication, Get.prototype.posts);
     this.router.get('/post/images/:page', authMiddleware.checkAuthentication, Get.prototype.postsWithImages);
+
     this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
+
+    this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
 
     return this.router;
   }
