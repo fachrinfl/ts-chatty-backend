b574ac85434c63302ac66716a1630d645f73ad57
Fachri Naufal <fachrinfl@Fachris-MacBook-Pro.local>
2023-04-24 12:14:13 +0700

    add remove reaction controller

---
+++ src/features/reaction/controllers/remove-reaction.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { IReactionJob } from '@reaction/interfaces/reaction.interface';
+import { ReactionsCache } from '@service/redis/reactions.cache';
+import { reactionQueue } from '@service/queues/reaction.queue';
+import { Helpers } from '@global/helpers/helpers';
+
+const reactionCache: ReactionsCache = new ReactionsCache();
+
+export class Remove {
+  public async reaction(req: Request, res: Response): Promise<void> {
+    const { postId, previousReaction, postReactions } = req.params;
+    await reactionCache.removePostReactionFromCache(postId, `${req.currentUser!.username}`, Helpers.parseJson(postReactions));
+    const databaseReactionData: IReactionJob = {
+      postId,
+      username: req.currentUser!.username,
+      previousReaction,
+    };
+    reactionQueue.addReactionJob('removeReactionFromDB', databaseReactionData);
+    res.status(HTTP_STATUS.OK).json({ message: 'Reaction remove from post' });
+  }
+}
